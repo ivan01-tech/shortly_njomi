7ac5ed4595073bd4003dc2b6a4f1b8afdf72a453
ivan01-tech <ivansilatsa@gmail.com>
2024-02-19 08:44:56 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/shortly-home/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
