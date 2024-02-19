2cc3f7214215c6f4a053254e347874cad4475e2c
ivan01-tech <ivansilatsa@gmail.com>
2024-02-19 09:00:01 +0100

    UPDATE package.json file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/shortly-home/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
